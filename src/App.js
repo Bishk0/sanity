2dae1d2cd08005df9182580d0091e0ba1a7b241a
Yurii Bishko <yurii.bishko@indeema.com>
2021-09-09 17:44:11 +0300

    sanity portfolio/blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import SinglePost from "./components/SinglePost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/post/:slug' component={SinglePost} />
+        <Route path='/post' component={Post} />
+        <Route path='/project' component={Project} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
